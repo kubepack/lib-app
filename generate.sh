bad6235a34310d40ac4794f3934d32eb2241d128
Tamal Saha <tamal@appscode.com>
2021-08-27 17:38:13 -0700

    Fix formatting
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- generate.sh
+++ generate.sh
@@ -1,43 +1,57 @@
 #!/bin/bash
 
+# Copyright AppsCode Inc. and Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 go run cmd/gen-simple-editor/main.go --all --skipExisting=false
 
 go run cmd/fuse-chart/*.go \
-  --sample-dir=/home/tamal/go/src/github.com/appscode/kubedb-samples/mongodb/sharded/prometheus.io/backupconfiguration/stash/tls/custom-auth/sharded \
-  --chart-dir=/home/tamal/go/src/go.bytebuilders.dev/ui-wizards/charts \
-  --sample-name=mongodb \
-  --resource.group=kubedb.com \
-  --resource.version=v1alpha2 \
-  --resource.name=mongodbs
+    --sample-dir=/home/tamal/go/src/github.com/appscode/kubedb-samples/mongodb/sharded/prometheus.io/backupconfiguration/stash/tls/custom-auth/sharded \
+    --chart-dir=/home/tamal/go/src/go.bytebuilders.dev/ui-wizards/charts \
+    --sample-name=mongodb \
+    --resource.group=kubedb.com \
+    --resource.version=v1alpha2 \
+    --resource.name=mongodbs
 
 go run cmd/fuse-chart/*.go \
-  --sample-dir=/home/tamal/go/src/github.com/appscode/kubedb-samples/postgres/cluster/prometheus.io/backupconfiguration/stash/tls/custom-auth/custom-auth-mode/custom-config/custom-pg-coordinator/custom-uid \
-  --chart-dir=/home/tamal/go/src/go.bytebuilders.dev/ui-wizards/charts \
-  --sample-name=postgres \
-  --resource.group=kubedb.com \
-  --resource.version=v1alpha2 \
-  --resource.name=postgreses
+    --sample-dir=/home/tamal/go/src/github.com/appscode/kubedb-samples/postgres/cluster/prometheus.io/backupconfiguration/stash/tls/custom-auth/custom-auth-mode/custom-config/custom-pg-coordinator/custom-uid \
+    --chart-dir=/home/tamal/go/src/go.bytebuilders.dev/ui-wizards/charts \
+    --sample-name=postgres \
+    --resource.group=kubedb.com \
+    --resource.version=v1alpha2 \
+    --resource.name=postgreses
 
 go run cmd/fuse-chart/*.go \
-  --sample-dir=/home/tamal/go/src/github.com/appscode/kubedb-samples/mariadb/cluster/prometheus.io/backupconfiguration/stash/tls/custom-auth/config-file/customize-pod-template \
-  --chart-dir=/home/tamal/go/src/go.bytebuilders.dev/ui-wizards/charts \
-  --sample-name=mariadb \
-  --resource.group=kubedb.com \
-  --resource.version=v1alpha2 \
-  --resource.name=mariadbs
+    --sample-dir=/home/tamal/go/src/github.com/appscode/kubedb-samples/mariadb/cluster/prometheus.io/backupconfiguration/stash/tls/custom-auth/config-file/customize-pod-template \
+    --chart-dir=/home/tamal/go/src/go.bytebuilders.dev/ui-wizards/charts \
+    --sample-name=mariadb \
+    --resource.group=kubedb.com \
+    --resource.version=v1alpha2 \
+    --resource.name=mariadbs
 
 go run cmd/fuse-chart/*.go \
-  --sample-dir=/home/tamal/go/src/github.com/appscode/kubedb-samples/elasticsearch/topology/prometheus.io/backupconfiguration/stash/cert-manager/custom-auth/custom-config/internal-users/kernel-settings \
-  --chart-dir=/home/tamal/go/src/go.bytebuilders.dev/ui-wizards/charts \
-  --sample-name=elasticsearch \
-  --resource.group=kubedb.com \
-  --resource.version=v1alpha2 \
-  --resource.name=elasticsearches
+    --sample-dir=/home/tamal/go/src/github.com/appscode/kubedb-samples/elasticsearch/topology/prometheus.io/backupconfiguration/stash/cert-manager/custom-auth/custom-config/internal-users/kernel-settings \
+    --chart-dir=/home/tamal/go/src/go.bytebuilders.dev/ui-wizards/charts \
+    --sample-name=elasticsearch \
+    --resource.group=kubedb.com \
+    --resource.version=v1alpha2 \
+    --resource.name=elasticsearches
 
 go run cmd/fuse-chart/*.go \
-  --sample-dir=/home/tamal/go/src/github.com/appscode/kubedb-samples/mysql/group-replication/prometheus.io/backupconfiguration/stash/tls/custom-auth/config-file/customize-pod-template \
-  --chart-dir=/home/tamal/go/src/go.bytebuilders.dev/ui-wizards/charts \
-  --sample-name=mysql \
-  --resource.group=kubedb.com \
-  --resource.version=v1alpha2 \
-  --resource.name=mysqls
+    --sample-dir=/home/tamal/go/src/github.com/appscode/kubedb-samples/mysql/group-replication/prometheus.io/backupconfiguration/stash/tls/custom-auth/config-file/customize-pod-template \
+    --chart-dir=/home/tamal/go/src/go.bytebuilders.dev/ui-wizards/charts \
+    --sample-name=mysql \
+    --resource.group=kubedb.com \
+    --resource.version=v1alpha2 \
+    --resource.name=mysqls
