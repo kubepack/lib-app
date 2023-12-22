4b7f21f9696dd6319c7baa24cc008e2a906c3a2d
Tamal Saha <tamal@appscode.com>
2023-12-21 22:04:00 -0800

    Add crossplane generator command
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- generate.sh
+++ generate.sh
@@ -299,3 +299,14 @@ go run cmd/fuse-chart/*.go \
     --resource.version=v1alpha1 \
     --resource.name=featuresets \
     --gen-crd=false
+
+go run cmd/fuse-chart/*.go \
+    --sample-dir=$HOME/go/src/go.bytebuilders.dev/ui-samples/featuresets/crossplane \
+    --chart-dir=$HOME/go/src/go.bytebuilders.dev/ui-wizards/charts \
+    --chart-version=$CHART_VERSION \
+    --sample-name=not-used \
+    --instance-name=crossplane \
+    --resource.group=ui.k8s.appscode.com \
+    --resource.version=v1alpha1 \
+    --resource.name=featuresets \
+    --gen-crd=false
