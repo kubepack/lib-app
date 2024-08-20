91917242e8ffb66d87991ae9721ba6691d5990e0
Arnob Kumar Saha <arnob@appscode.com>
2024-08-20 17:51:50 +0600

    Add kubestash restoresession (#102)
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>

--- kubestash.sh
+++ kubestash.sh
@@ -25,11 +25,11 @@ go run cmd/fuse-chart/*.go \
     --resource.version=v1alpha1 \
     --resource.name=backupstorages
 
-# go run cmd/fuse-chart/*.go \
-#     --sample-dir=$HOME/go/src/go.bytebuilders.dev/ui-samples/restoresession/custom \
-#     --chart-dir=$HOME/go/src/go.bytebuilders.dev/ui-wizards/charts \
-#     --chart-version=$CHART_VERSION \
-#     --sample-name=restore-app \
-#     --resource.group=stash.appscode.com \
-#     --resource.version=v1beta1 \
-#     --resource.name=restoresessions
+go run cmd/fuse-chart/*.go \
+   --sample-dir=$HOME/go/src/go.bytebuilders.dev/ui-samples/kubestash/restoresession/custom \
+   --chart-dir=$HOME/go/src/go.bytebuilders.dev/ui-wizards/charts \
+   --chart-version=$CHART_VERSION \
+   --sample-name=restore-app \
+   --resource.group=core.kubestash.com \
+   --resource.version=v1alpha1 \
+   --resource.name=restoresessions
