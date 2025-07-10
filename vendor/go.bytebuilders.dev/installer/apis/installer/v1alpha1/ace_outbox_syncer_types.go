f3ac7f2aeb6a0a7e04e658d6906fc989bcbb6bef
1gtm <1gtm@appscode.com>
2025-07-10 00:15:56 +0000

    Update for release ACE@v2025.7.16
    ProductLine: ACE
    
    Release: v2025.7.16
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- vendor/go.bytebuilders.dev/installer/apis/installer/v1alpha1/ace_outbox_syncer_types.go
+++ vendor/go.bytebuilders.dev/installer/apis/installer/v1alpha1/ace_outbox_syncer_types.go
@@ -72,11 +72,11 @@ type OutboxSyncerSpec struct {
 	Tolerations []core.Toleration `json:"tolerations"`
 	// If specified, the pod's scheduling constraints
 	// +optional
-	Affinity     *core.Affinity     `json:"affinity"`
-	Volumes      []core.Volume      `json:"volumes"`
-	VolumeMounts []core.VolumeMount `json:"volumeMounts"`
-	Infra        AceInfra           `json:"infra"`
-	Settings     AceAPISettings     `json:"settings"`
+	Affinity     *core.Affinity          `json:"affinity"`
+	Volumes      []core.Volume           `json:"volumes"`
+	VolumeMounts []core.VolumeMount      `json:"volumeMounts"`
+	Infra        AceInfra                `json:"infra"`
+	Settings     AceOutboxSyncerSettings `json:"settings"`
 	// List of sources to populate environment variables in the container.
 	// The keys defined within a source must be a C_IDENTIFIER. All invalid keys
 	// will be reported as an event when the container is starting. When a key exists in multiple
@@ -94,6 +94,18 @@ type OutboxSyncerSpec struct {
 	// +listType=map
 	// +listMapKey=name
 	Env []core.EnvVar `json:"env"`
+}
+
+type AceOutboxSyncerSettings struct {
+	CAProviderClass                   string `json:"caProviderClass"`
+	NatsMountPath                     string `json:"natsMountPath"`
+	AceOutboxSyncerSettingsSecretName `json:"secretName"`
+}
+
+type AceOutboxSyncerSettingsSecretName struct {
+	PlatformConfig       string `json:"platformConfig"`
+	PlatformSystemConfig string `json:"platformSystemConfig"`
+	NatsCred             string `json:"natsCred"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
